252d3a382fa4892ffb31cb10bf57f72b78fb8422
Joberoi23 <jobe0005@student.monash.edu>
2024-10-27 13:31:46 +1100

    adding pkgdwon_yml

--- IndianLiteracyRates/R/literacy_growth.R
+++ IndianLiteracyRates/R/literacy_growth.R
@@ -1,24 +1,25 @@
 #' Calculate Literacy Growth (2001-2011)
 #'
 #' @description This function calculates the percentage growth in literacy rates
-#' from 2001 to 2011 for each state or union territory in the data set.
+#' from 2001 to 2011 for each state or union territory in the dataset.
 #'
-#' @param A data frame containing literacy rates from the GOI data set.
+#' @param data A data frame containing literacy rates from the GOI dataset.
 #' @return A data frame with the original data and an additional column, `literacy_growth`, showing the growth rate.
 #' @examples
 #' literacy_growth(data = GOI_data)
 #' @export
 literacy_growth <- function(data) {
-  # Checking if the necessary columns are present in the data set
+  # Check if the necessary columns are present in the dataset
   if (!all(c("literacy_rate_persons_total_2001", "literacy_rate_persons_total_2011") %in% colnames(data))) {
     stop("Data must contain 'literacy_rate_persons_total_2001' and 'literacy_rate_persons_total_2011' columns.")
   }
 
-  # Calculating literacy growth
+  # Calculate literacy growth
   data$literacy_growth <- (data$literacy_rate_persons_total_2011 - data$literacy_rate_persons_total_2001) /
     data$literacy_rate_persons_total_2001 * 100
   data
 }
+
 
 
 
