ef1b1d27206eb1cbf16f0da6b9e9afb6007a1d8c
Joberoi23 <jobe0005@student.monash.edu>
2024-10-27 13:46:04 +1100

    building the site

--- IndianLiteracyRates/R/literacy_growth.R
+++ IndianLiteracyRates/R/literacy_growth.R
@@ -1,20 +1,14 @@
 #' Calculate Literacy Growth (2001-2011)
 #'
-#' @description This function calculates the percentage growth in literacy rates
-#' from 2001 to 2011 for each state or union territory in the dataset.
-#'
-#' @param data A data frame containing literacy rates from the GOI dataset.
-#' @return A data frame with the original data and an additional column, `literacy_growth`, showing the growth rate.
-#' @examples
-#' literacy_growth(data = GOI_data)
+#' @description Calculates the growth in literacy rates from 2001 to 2011.
+#' @param data A data frame with literacy rates for 2001 and 2011.
+#' @return The input data frame with an additional `literacy_growth` column.
 #' @export
 literacy_growth <- function(data) {
-  # Check if the necessary columns are present in the dataset
   if (!all(c("literacy_rate_persons_total_2001", "literacy_rate_persons_total_2011") %in% colnames(data))) {
     stop("Data must contain 'literacy_rate_persons_total_2001' and 'literacy_rate_persons_total_2011' columns.")
   }
 
-  # Calculate literacy growth
   data$literacy_growth <- (data$literacy_rate_persons_total_2011 - data$literacy_rate_persons_total_2001) /
     data$literacy_rate_persons_total_2001 * 100
   data
